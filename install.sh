b3f0435be6aca15bfea3b7cb388f678a492701c4
Marcel Scheck <m.scheck@efg.gg>
2023-11-05 20:31:13 +0100

    initial commit

---
+++ install.sh
@@ -0,0 +1,14 @@
+# Updates
+sudo apt update
+sudo apt upgrade
+
+# Basics
+sudo apt install vim htop
+
+# Kubernetes
+
+sudo apt install containerd
+
+## TODO disable swap
+
+## TODO enable IP-Forward
